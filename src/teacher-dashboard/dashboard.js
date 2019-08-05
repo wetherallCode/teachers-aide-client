645ca716a19fbf2675b5565f3adb50ca7e059e17
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-04 20:33:38 -0400

    trying to make this work

--- src/teacher-dashboard/dashboard.js
+++ src/teacher-dashboard/dashboard.js
@@ -9,7 +9,7 @@ import Classes from './class-tool-components/Classes'
 import SchoolDay from './school-day/SchoolDay'
 import LessonPlanner from './lesson-planner/LessonPlanner'
 import { gql } from 'apollo-boost'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import { useQuery } from '@apollo/react-hooks'
 import UserLogin from './UserLogin'
 import Logout from './Logout'
 // import * as ApolloTypes from './__generated__/getAllStudents'
@@ -33,50 +33,58 @@ const DashBoardNavigation = ({ match }) => {
 	return (
 		<>
 			{!me ? (
-				<>
-					<UserLogin />
-					<h1>{me}</h1>
-				</>
+				<UserLogin />
 			) : (
-				<div>
-					<header className='Header' style={{ borderBottom: '3px solid var(--white)' }}>
-						<Link style={{ padding: '2%' }} to='/'>
-							MrWetherall.org
-						</Link>
-						<div style={{ display: 'flex', alignContent: 'center', justifyContent: 'flex-end' }}>
-							<Link style={{ margin: '1%' }} to={`${match.url}/roster-view`}>
-								Rosters
-							</Link>
-							<Link style={{ margin: '1%' }} to={`${match.url}/classroom/class-period-selector`}>
-								Class
-							</Link>
-							<Link style={{ margin: '1%' }} to={`${match.url}/lesson-planner/`}>
-								Lesson Planner
-							</Link>
-							<Link style={{ margin: '1%' }} to={`${match.url}/school-day`}>
-								School Day
-							</Link>
-							<Link style={{ margin: '1%' }} to={`${match.url}/logout`}>
-								Logout
-							</Link>
+				<>
+					{me && (
+						<div>
+							<header className='Header' style={{ borderBottom: '3px solid var(--white)' }}>
+								<Link style={{ padding: '2%' }} to='/'>
+									MrWetherall.org
+								</Link>
+								<div
+									style={{ display: 'flex', alignContent: 'center', justifyContent: 'flex-end' }}>
+									<Link style={{ margin: '1%' }} to={`${match.url}/roster-view`}>
+										Rosters
+									</Link>
+									<Link
+										style={{ margin: '1%' }}
+										to={`${match.url}/classroom/class-period-selector`}>
+										Class
+									</Link>
+									<Link style={{ margin: '1%' }} to={`${match.url}/lesson-planner/`}>
+										Lesson Planner
+									</Link>
+									<Link style={{ margin: '1%' }} to={`${match.url}/school-day`}>
+										School Day
+									</Link>
+									<Link style={{ margin: '1%' }} to={`${match.url}/logout`}>
+										Logout
+									</Link>
+								</div>
+							</header>
+
+							<div id='test' style={{ display: 'grid', gridTemplateColumns: '1fr 4fr' }}>
+								<Route path={`${match.path}/roster-view/`} component={RosterNavBar} />
+								<Route path={`${match.path}/roster-view/:periodName`} component={Rosters} />
+							</div>
+
+							<Route exact path='/dashboard/roster-view/allStudent' component={AllStudentRoster} />
+							<Route
+								exact
+								path='/dashboard/roster-profile/student/:studentInfo'
+								component={Student}
+							/>
+							<Route path='/dashboard/rosters/addNewStudent' component={StudentInfoLoader} />
+
+							<Route path={`${match.path}/classroom/`} component={Classes} />
+							<Route path={`${match.path}/lesson-planner/`} component={LessonPlanner} />
+
+							<Route path={`${match.path}/school-day`} component={SchoolDay} />
+							<Route path={`${match.path}/logout`} component={Logout} />
 						</div>
-					</header>
-
-					<div id='test' style={{ display: 'grid', gridTemplateColumns: '1fr 4fr' }}>
-						<Route path={`${match.path}/roster-view/`} component={RosterNavBar} />
-						<Route path={`${match.path}/roster-view/:periodName`} component={Rosters} />
-					</div>
-
-					<Route exact path='/dashboard/roster-view/allStudent' component={AllStudentRoster} />
-					<Route exact path='/dashboard/roster-profile/student/:studentInfo' component={Student} />
-					<Route path='/dashboard/rosters/addNewStudent' component={StudentInfoLoader} />
-
-					<Route path={`${match.path}/classroom/`} component={Classes} />
-					<Route path={`${match.path}/lesson-planner/`} component={LessonPlanner} />
-
-					<Route path={`${match.path}/school-day`} component={SchoolDay} />
-					<Route path={`${match.path}/logout`} component={Logout} />
-				</div>
+					)}
+				</>
 			)}
 		</>
 	)
