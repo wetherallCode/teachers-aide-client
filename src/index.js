58d493e85c2a565f2b25f093d8e01f0e3abfb05e
Rob Wetherall <robertgwetherall1@gmail.com>
2019-08-15 15:22:12 -0400

    back to herokuapp

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ const defaultState = {
 }
 
 const client = new ApolloClient({
-	uri: `/graphql`,
-	credentials: 'same-origin',
-	// uri: 'https://mrwetherall.herokuapp.com/graphql',
-	// credentials: 'include',
+	// uri: `/graphql`,
+	// credentials: 'same-origin',
+	uri: 'https://mrwetherall.herokuapp.com/graphql',
+	credentials: 'include',
 	clientState: {
 		defaults: defaultState,
 		resolvers: {}
