59824ac204705b820545404ef9b1e513522ed70b
Rob Wetherall <moosemacbook@Robs-MacBook-Pro.local>
2020-04-03 10:49:23 -0400

    removed superfluous useEffect declaration

--- src/teacher-dashboard/class-tool-components/ClassRoom.js
+++ src/teacher-dashboard/class-tool-components/ClassRoom.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Route } from 'react-router'
 import DeskSelector from './DeskSelector'
 import ClassRoomTools from './ClassRoomTools'
@@ -7,118 +7,125 @@ import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 
 export const FIND_CLASSPERIOD_FOR_CLASSROOM = gql`
-	query findClassPeriodForClassRoom($assignedDate: Date, $period: periodName) {
-		findClassPeriod(assignedDate: $assignedDate, period: $period) {
-			_id
-			assignedLesson {
-				_id
-				lessonName
-				inUnit {
-					name
-				}
-				warmup
-				essentialQuestion {
-					question
-				}
-				readings {
-					pages
-					sections
-				}
-				socraticQuestions {
-					question
-					type
-				}
-				vocabWords {
-					word
-					partOfSpeech
-					definition
-				}
-				studyGuideQuestions
-			}
-			period
-			assignedDate
-			assignedHomework {
-				assignedDate
-				dueDate
-				assignmentType
-				markingPeriod
-				readingPages
-				readingSections
-				maxScore
-			}
-			assignedTest {
-				assignedDate
-				assignmentType
-				dueDate
-				markingPeriod
-				maxScore
-				readingPages
-				readingSections
-			}
-			assignedProtocols {
-				... on SocraticQuestionProtocolForClassPeriod {
-					isActive
-					socraticQuestion
-					socraticQuestionType
-				}
-			}
-			livePeriod
-		}
-	}
+  query findClassPeriodForClassRoom($assignedDate: Date, $period: periodName) {
+    findClassPeriod(assignedDate: $assignedDate, period: $period) {
+      _id
+      assignedLesson {
+        _id
+        lessonName
+        inUnit {
+          name
+        }
+        warmup
+        essentialQuestion {
+          question
+        }
+        readings {
+          pages
+          sections
+        }
+        socraticQuestions {
+          question
+          type
+        }
+        vocabWords {
+          word
+          partOfSpeech
+          definition
+        }
+        studyGuideQuestions
+      }
+      period
+      assignedDate
+      assignedHomework {
+        assignedDate
+        dueDate
+        assignmentType
+        markingPeriod
+        readingPages
+        readingSections
+        maxScore
+      }
+      assignedTest {
+        assignedDate
+        assignmentType
+        dueDate
+        markingPeriod
+        maxScore
+        readingPages
+        readingSections
+      }
+      assignedProtocols {
+        ... on SocraticQuestionProtocolForClassPeriod {
+          isActive
+          socraticQuestion
+          socraticQuestionType
+        }
+      }
+      livePeriod
+    }
+  }
 `
 
 const ClassRoom = ({ match }) => {
-	const [presentStudents, setPresentStudents] = useState([])
+  const [presentStudents, setPresentStudents] = useState([])
 
-	const todaysDate = new Date().toISOString().substring(0, 10)
-	const { periodName } = match.params
+  const todaysDate = new Date().toISOString().substring(0, 10)
+  const { periodName } = match.params
 
-	const { data, loading, error, startPolling, stopPolling } = useQuery(
-		FIND_CLASSPERIOD_FOR_CLASSROOM,
-		{
-			variables: { period: periodName, assignedDate: todaysDate }
-			// pollInterval: 10
-		}
-	)
-	if (loading) return null
-	if (error) console.log(error)
+  const { data, loading, error, startPolling, stopPolling } = useQuery(
+    FIND_CLASSPERIOD_FOR_CLASSROOM,
+    {
+      variables: { period: periodName, assignedDate: todaysDate }
+      // pollInterval: 10
+    }
+  )
+  if (loading) return null
+  if (error) console.log(error)
 
-	const { url } = match
+  const { url } = match
 
-	return (
-		<>
-			<div
-				style={{
-					backgroundColor: 'var(--white)',
-					display: 'grid',
-					gridTemplateRows: '3fr 1fr',
-					gridGap: '1px'
-					// height: '100vh',
-					// overflow: 'scroll'
-				}}>
-				<DeskSelector
-					match={match}
-					periodName={periodName}
-					todaysDate={todaysDate}
-					setPresentStudents={setPresentStudents}
-				/>
-				{data.findClassPeriod !== 'undefined' && (
-					<ClassRoomTools
-						classPeriodInfo={data.findClassPeriod}
-						presentStudents={presentStudents}
-						period={periodName}
-						match={match}
-						todaysDate={todaysDate}
-						startPolling={startPolling}
-						stopPolling={stopPolling}
-					/>
-				)}
-			</div>
-			<Route
-				path={`${url}/:deskNumber`}
-				render={props => <StudentInfo {...props} periodName={periodName} todaysDate={todaysDate} />}
-			/>
-		</>
-	)
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: 'var(--white)',
+          display: 'grid',
+          gridTemplateRows: '3fr 1fr',
+          gridGap: '1px'
+          // height: '100vh',
+          // overflow: 'scroll'
+        }}
+      >
+        <DeskSelector
+          match={match}
+          periodName={periodName}
+          todaysDate={todaysDate}
+          setPresentStudents={setPresentStudents}
+        />
+        {data.findClassPeriod !== 'undefined' && (
+          <ClassRoomTools
+            classPeriodInfo={data.findClassPeriod}
+            presentStudents={presentStudents}
+            period={periodName}
+            match={match}
+            todaysDate={todaysDate}
+            startPolling={startPolling}
+            stopPolling={stopPolling}
+          />
+        )}
+      </div>
+      <Route
+        path={`${url}/:deskNumber`}
+        render={props => (
+          <StudentInfo
+            {...props}
+            periodName={periodName}
+            todaysDate={todaysDate}
+          />
+        )}
+      />
+    </>
+  )
 }
 export default ClassRoom
